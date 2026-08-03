5e7459164470ac45b83208ca7e8aba3113cc4b0d
Karl Herrick <me@karlherrick.com>
2026-08-02 23:00:50 -0500

    resolve issue: 1
    - remove environment variable
    - remove main build script
    - remove news-summary JSON schema
    - remove scheduled build
    - stop deleting the first and last line of README.md
    - stop running npm start script to build README.md
    - use node24

--- bin/build.sh
+++ bin/build.sh
@@ -3,11 +3,9 @@
 # change directory to the project root
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.." || exit 1
 
-npm start --silent > README.md \
-  && sed -i 's/[[:space:]]*$//' README.md \
+sed -i 's/[[:space:]]*$//' README.md \
   && cat -s README.md > README.md.tmp \
   && mv README.md.tmp README.md \
-  && sed -i '1d;$d' README.md \
   && npm run build:html \
   && sed -i '1s;^;<body>\n;' ./content.html \
   && echo '</body>' >> ./content.html \
